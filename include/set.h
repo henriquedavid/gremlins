c3dd1d9c2baf9d6e5461b29eaab77c2bc1710a06
Henrique David de Medeiros <henriquemed101@gmail.com>
2018-06-26 09:10:46 -0300

    Inclusão do set

--- include/set.h
+++ include/set.h
@@ -1,10 +1,32 @@
 #ifndef SET_H
 #define SET_H
 
+template < typename T >
 class set
 {
 public:
-    set();
+    set(int size_ = 1);						//!< Construtor.
+    ~set();									// Destrutor.
+    T * insert( const T & value );			// Inserir um elemento de forma que fique ordenado.
+    void clear();							// Limpar todo o conjunto.
+    T * begin() const;						// Retornar o início do vetor.
+    T * end() const;						// Retornar o fim do vetor.
+    const T * cend();						// Retornar o fim do vetor cosntante.
+    void erase( T * pos );					// Remover um determinado elemento em uma posição.
+    T * upper_bound( const T & value );		// Retornar o próximo maior valor que value.
+    bool empty();							// Verificar se o vetor está vazio.
+    int size();								// Retornar o tamanho do vetor.
+
+    void print();							// Imprimir os elementos para fins de depuração.
+
+    void operator=( const set & value );		// Operador de atribuição
+
+private:
+	T * m_area;
+	int m_size;
+	int m_capacity;
 };
+
+#include "../src/set.cpp"
 
 #endif // SET_H
--- src/set.cpp
+++ src/set.cpp
@@ -1,6 +1,182 @@
-//#include "include/set.h"
+#include "../include/set.h"
 
-//set::set()
-//{
+/// Construtor padrão.
+template < typename T >
+set<T>::set(int size_)
+{
+	m_area = new T[size_];
+	m_size = 0;
+	m_capacity = size_;
+}
 
-//}
+template < typename T >
+set<T>::~set(){
+	delete [] m_area;
+}
+
+/// Insere elementos no set de forma que eles fiquem ordenados por tamanho.
+template < typename T >
+T * set<T>::insert(  const T & value ){
+	if(m_size >= m_capacity){
+		// Deve tratar o caso de ser necessário novo espaço?
+		T * new_storage = new T[m_capacity * 2];
+		m_capacity *= 2;
+
+		auto b_(m_area);
+		auto new_b(new_storage);
+		auto e_(m_area+m_size);
+
+		while(b_ != e_){
+			*new_b = *b_;
+			b_++;
+			new_b++;
+		}
+
+		delete [] m_area;
+		m_area = new_storage;
+	}
+
+	auto begin_(m_area);
+	auto end_(m_area+m_size);
+
+	// Caso de ser o primeiro elemento.
+	if( m_size == 0 ){
+		*begin_ = value;
+		m_size++;
+		return begin_;
+	}
+
+	// Procura o local que será inserido (ficar ordenado).
+	while( begin_ != end_ && value > *begin_ ){
+		begin_++;
+	}
+
+	// Local onde deve ser adicionado.
+	auto aux(begin_);
+	
+	// Após encontrar o local ele vai mover todos os elementos até aquele ponto para um espaço a frente para abrir espaço.
+	while( end_ != aux ){
+
+		// Troca os elementos.
+		std::swap(*end_, *(end_-1));
+
+		end_--;
+
+	}
+
+	// Insere o elemento no local correto.
+	*aux = value;
+
+	// Aumenta o tamanho
+	m_size++;
+
+	// Retorna a posição do valor inserido.
+	return aux;
+}
+
+/// Retorna o primeiro espaço.
+template < typename T >
+T * set<T>::begin() const{
+	return m_area;
+}
+
+/// Retorna o final do set.
+template < typename T >
+T * set<T>::end() const{
+	return m_area + m_size;
+}
+
+/// Retorna o final do set.
+template < typename T >
+const T * set<T>::cend(){
+	return m_area + m_size;
+}
+
+/// Verificar se está vazio.
+template < typename T >
+bool set<T>::empty(){
+	return m_size == 0;
+}
+
+/// Retornar o espaço de armazenamento
+template < typename T >
+int set<T>::size(){
+	return m_size;
+}
+
+template < typename T >
+void set<T>::print(){
+	auto b(m_area);
+	auto e(m_area+m_size);
+
+	std::cout << "Set = [ ";
+
+	if( m_size == 0 ){
+		std::cout << " vazio ]\n";
+		return;
+	}
+
+	while(b != e){
+		std::cout << *b << " ";
+		b++;
+	}
+
+	std::cout << "]" << std::endl;
+}
+
+template < typename T >
+T* set<T>::upper_bound( const T & value ){
+	auto b_(m_area);
+	auto e_(m_area+m_size);
+
+	while(b_ != e_){
+
+		if(value < *b_)
+			return b_;
+
+		b_++;
+	}
+
+	return b_;
+
+}
+
+template < typename T >
+void set<T>::clear(){
+
+	delete [] this->m_area;
+    this->m_area = new T[1];
+    this->m_capacity = 1;
+    this->m_size = 0;
+
+}
+
+template < typename T >
+void set<T>::erase( T * pos ){
+
+	auto b_(pos);
+	auto e_(m_area+m_size);
+
+	while(b_ != e_){
+		std::swap(*b_, *(b_+1));
+		b_++;
+	}
+
+	m_size--;
+
+}
+
+template < typename T >
+void set<T>::operator=( const set & value ){
+   	m_capacity = value.m_capacity;
+   	m_size = value.m_size;
+
+   	m_area = new T[m_capacity];
+
+   	auto beg(m_area);
+
+   	for( auto i(value.begin()) ; i < value.end() ; i++ ){
+   		*beg = *i;
+   		beg++;
+   	}
+}
